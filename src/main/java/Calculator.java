2f70a34b5e44582de0f4cd25d6a45e0c4c1f297c
Ильичев Дмитрий <ili4off06@gmail.com>
2026-05-01 11:32:56 +0300

    Реализация методов калькулятора и solver

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,24 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) return 0;
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
+
     public int solver(){
-        //TODO inser your realisation in method solver
+
+        int sum = add(5, 3);
+        int diff = dif(10, 2);
+        int product = times(sum, diff);
+        int result = div(product, 4);
+        return result;
     }
 }
