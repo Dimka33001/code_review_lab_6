66f40b7148baa3745964f66c190367d8e22ca7fe
Dimka33001 <ili4off06@gmail.com>
2026-05-17 15:32:52 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,23 +2,24 @@ public class Calculator {
     public int add(int a, int b){
         return a + b;
     }
+    
     public int dif(int a, int b){
         return a - b;
     }
+    
     public int div(int a, int b){
         if (b == 0) {
             throw new IllegalArgumentException("Деление на ноль!");
         }
-        if (b == 0) return 0;
         return a / b;
     }
+    
     public int times(int a, int b){
         return a * b;
     }
 
     public int solver(){
-        return dif(div(add(15, 3), 2), times(5, 1));
-
+        
         int sum = add(5, 3);
         int diff = dif(10, 2);
         int product = times(sum, diff);
